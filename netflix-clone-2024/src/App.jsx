d6dd34f3a36cf3ab8482901d809f689e7ad5a0b0
BEKMAL <bekalumsc@gmail.com>
2024-09-08 10:14:39 +0300

    header and footer added

--- netflix-clone-2024/src/App.jsx
+++ netflix-clone-2024/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Home from './Pages/Home/Home'
 
 function App() {
  
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className='App'>
+      <Home/>
+    </div>
   )
 }
 
